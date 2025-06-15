3638cc8f28b5018adc763005d517c8da809fee70
RUSHIKESH <RUSHIKESH@LAPTOP-JCJ0K9B6>
2025-06-15 15:17:42 +0530

    commit question services

---
+++ QuestionService/src/main/java/com/question/repo/QuestionRepo.java
@@ -0,0 +1,12 @@
+package com.question.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.question.entities.Question;
+
+@Repository
+public interface QuestionRepo  extends JpaRepository<Question, Integer>{
+	
+
+}
