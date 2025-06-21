0246f8d9e6bfab985b63feacc666a04bd99824f2
RUSHIKESH <RUSHIKESH@LAPTOP-JCJ0K9B6>
2025-06-21 17:27:54 +0530

    new commit 21/06

--- QuestionService/src/main/java/com/question/QuestionServiceApplication.java
+++ QuestionService/src/main/java/com/question/QuestionServiceApplication.java
@@ -2,8 +2,10 @@ package com.question;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
